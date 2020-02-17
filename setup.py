257634325f5f92835d6c755912f70c8be48386fd
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-02-17 11:25:29 -0800

    Add python setup.py to GeoBounce

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    "description": "Platformer game with a bit of geometry in it",
+    "author": "Boredom Games",
+    "url": "https://github.com/boredomgames/GeoBounce",
+    "download_url": "https://github.com/boredomgames/GeoBounce",
+    "version": "0.1.0",
+    "install_requires": [],
+    "packages": ["GeoBounce"],
+    "scripts": [],
+    "name": "GeoBounce",
+    "python_requires": ">=3.6",
+}
+
+setup(**config)
