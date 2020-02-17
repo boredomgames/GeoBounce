99cd69fd42d50cc07d66f981ff64dd81eb7b394a
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-02-16 20:38:07 -0800

    Create frames timer

---
+++ GeoBounce/timer.py
@@ -0,0 +1,12 @@
+import time
+
+class Timer(object):
+    def __init__(self):
+        self.time = time.time()
+
+    def tick(self, interval=60):
+        if time.time() - (1 / interval) >= self.time:
+            self.time = time.time()
+            return True
+        else:
+            return False
