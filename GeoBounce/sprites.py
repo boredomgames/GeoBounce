dde51eed756a72b91f55600c8328dd38b1a745f8
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-02-16 20:38:23 -0800

    Create proper sprites

--- GeoBounce/sprites.py
+++ GeoBounce/sprites.py
@@ -1,25 +1,120 @@
-class GeoBox(object):
-    def __init__(self, canvas, position):
-        self.canvas = canvas
-        self.position = list(position)
-        self.tag = None
-        self.draw()
+class Sprite(object):
+    def __init__(self, game, position, dimensions, color):
+        self._game = game
+        self._position = position
+        self._dimensions = dimensions
+        self._color = color
+        self._tag = None
 
     def draw(self):
-        self.tag = self.canvas.create_rectangle(
-            *self.position,
-            50,
-            50,
-            fill="blue",
-            outline="blue",
-        ) # x, y, width, height
+        self._tag = self._game._canvas.create_rectangle(
+            *self._position,
+            *[x + y for x, y in zip(self._position, self._dimensions)],
+            fill=self._color,
+            outline=self._color,
+        )
 
-    def move_by(self, new_position):
-        self.position[0] += new_position[0]
-        self.position[1] += new_position[1]
-        self.canvas.move(self.tag, *new_position)
+    def move(self, new_position):
+        self._position[0] += new_position[0]
+        self._position[1] += new_position[1]
+        self._game._canvas.move(self._tag, *new_position)
 
-    def move_to(self, new_position):
-        self.position[0] = new_position[0]
-        self.position[1] = new_position[1]
-        self.canvas.coords(self.tag, *self.position, 50, 50)
+    def collide(self):
+        return self._game._canvas.find_overlapping(*self._position, *[x + y for x, y in zip(self._position, self._dimensions)])
+
+
+class Obstacle(Sprite):
+    def __init__(self, game, position, dimensions, color):
+        super().__init__(game, position, dimensions, color)
+
+
+class Item(Sprite):
+    def __init__(self, game, position, dimensions, color):
+        super().__init__(game, position, dimensions, color)
+
+class Player(Sprite):
+    def __init__(self, game, position, dimensions, color):
+        super().__init__(game, position, dimensions, color)
+        self.jumping = False
+        self.jump_index = 0
+        self.jump_path = [
+            -15,
+            -14,
+            -13,
+            -12,
+            -11,
+            -10,
+            -9,
+            -8,
+            -7,
+            -6,
+            -5,
+            -4,
+            -3,
+            -2,
+            -1,
+            -0,
+            0,
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            12,
+            13,
+            14,
+            15,
+        ]
+
+        self._game.on("<space>")(lambda x: self.jump())
+
+    def jump(self):
+        self.jumping = True
+
+    def run_jump(self):
+        try:
+            if self.jumping:
+                self.move([0, self.jump_path[self.jump_index]])
+                self.jump_index += 1
+        except IndexError:
+            self.jumping = False
+            self.jump_index = 0
+
+
+# class GeoBox(object):
+#     def __init__(self, game):
+#         self.game = game
+#         self.canvas = game._canvas
+#         self.position = GEOBOX_POSITION
+#         self.tag = None
+#         self.collide = False
+#         self.draw()
+
+#     def draw(self):
+#         self.tag = self.game.create_rectangle(
+#             *self.position,
+#             50,
+#             50,
+#             fill="blue",
+#             outline="blue",
+#         ) # x, y, width, height
+
+#     def move_by(self, new_position):
+#         self.position[0] += new_position[0]
+#         self.position[1] += new_position[1]
+#         self.canvas.move(self.tag, *new_position)
+
+#     def move_to(self, new_position):
+#         self.position[0] = new_position[0]
+#         self.position[1] = new_position[1]
+#         self.canvas.coords(self.tag, *self.position, self.position[0] + 50, self.position[1] + 50)
+
+#     def jump(self):
+#         for item in coords:
+#         self.move_by([0, 1])
