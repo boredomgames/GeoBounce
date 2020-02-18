9f7255264d017405e0c4df258385a4d52c157a91
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-02-17 16:46:03 -0800

    Migrate levels to a separate file

---
+++ GeoBounce/levels.py
@@ -0,0 +1,50 @@
+level1 = [
+    {
+        "type": "player",
+        "position": [390, 380],
+        "dimensions": [20, 20],
+        "color": "blue",
+    },
+    {
+        "type": "surface",
+        "position": [0, 400],
+        "dimensions": [800, 200],
+        "color": "black",
+    },
+    {
+        "type": "obstacle",
+        "position": [600, 380],
+        "dimensions": [20, 20],
+        "color": "red"
+    },
+    {
+        "type": "obstacle",
+        "position": [780, 380],
+        "dimensions": [20, 20],
+        "color": "red"
+    },
+    {
+        "type": "obstacle",
+        "position": [900, 580],
+        "dimensions": [20, 20],
+        "color": "red",
+    },
+    {
+        "type": "surface",
+        "position": [1200, 580],
+        "dimensions": [100, 20],
+        "color": "black",
+    },
+    {
+        "type": "reward",
+        "position": [1220, 560],
+        "dimensions": [10, 10],
+        "color": "yellow",
+    },
+    {
+        "type": "surface",
+        "position": [1300, 560],
+        "dimensions": [100, 40],
+        "color": "black"
+    }
+]
