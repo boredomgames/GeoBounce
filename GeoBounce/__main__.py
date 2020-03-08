4aacf8de9ef255c4a3e3d565bf67a1bb90b50b37
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-03-08 15:17:38 -0700

    Update __main__ to use new level system

--- GeoBounce/__main__.py
+++ GeoBounce/__main__.py
@@ -1,13 +1,22 @@
 from .game import Game
-from .level import Level
+from .level import LevelOld, Level
 from .levels import level1
+
+
+# def main():
+#     game = Game()
+#     level = LevelOld(game, "Level 1", level1)
+#     # level._testmode = True  # FOR TESTING ONLY
+#     level.initialize()
+#     game.play()
+#     level.run()
 
 
 def main():
     game = Game()
-    level = Level(game, "Level 1", level1)
-    # level._testmode = True  # FOR TESTING ONLY
-    level.initialize()
+    level = Level(game, "Level 1", level1, legacy=True)
+    level.generate()
+    level.draw()
     game.play()
     level.run()
 
