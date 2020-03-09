8aabe51f01f8aeb0cc135f2bbe1cb61220f737d9
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-03-08 20:05:20 -0700

    Run Level 2 by default

--- GeoBounce/__main__.py
+++ GeoBounce/__main__.py
@@ -1,25 +1,15 @@
 from .game import Game
 from .level import LevelOld, Level
-from .levels import level1
-
-
-# def main():
-#     game = Game()
-#     level = LevelOld(game, "Level 1", level1)
-#     # level._testmode = True  # FOR TESTING ONLY
-#     level.initialize()
-#     game.play()
-#     level.run()
-
+from .levels import level1, level2
 
 def main():
     game = Game()
-    level = Level(game, "Level 1", level1, legacy=True)
+    # level = Level(game, "Level 1", level1)
+    level = Level(game, "Level 2", level2)
     level.generate()
     level.draw()
     game.play()
     level.run()
-
 
 if __name__ == "__main__":
     main()
