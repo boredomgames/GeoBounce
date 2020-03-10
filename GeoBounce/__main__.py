4679b0608049a5913eb6793fc1c48799159bc12c
Python Minecraft Raspberry Pi <palm.tree6555365@gmail.com>
2020-03-10 18:58:37 +0000

    Option to select level 1 instead of default level 2

--- GeoBounce/__main__.py
+++ GeoBounce/__main__.py
@@ -1,11 +1,14 @@
 from .game import Game
 from .level import LevelOld, Level
 from .levels import level1, level2
+import sys
 
 def main():
     game = Game()
-    # level = Level(game, "Level 1", level1)
-    level = Level(game, "Level 2", level2)
+    if len(sys.argv) == 2 and sys.argv[1] == "lvl1":
+        level = Level(game, "Level 1", level1)
+    else:
+        level = Level(game, "Level 2", level2)
     level.generate()
     level.draw()
     game.play()
