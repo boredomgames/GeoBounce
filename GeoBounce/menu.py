6745c5bc913be817f181719de16cc8b688805321
Python Minecraft Raspberry Pi <palm.tree6555365@gmail.com>
2020-03-11 15:09:08 +0000

    More progress on menu code, trying to keep code style consistent

--- GeoBounce/menu.py
+++ GeoBounce/menu.py
@@ -1,4 +1,4 @@
-class StartMenu(object):
+class Menu(object):
     def __init__(self, items, is_open=True):
         self._items = items
         self.is_open = is_open
@@ -11,16 +11,37 @@ class StartMenu(object):
         self.is_open = True
     def menu_defocus(self):
         self.is_open = False
+    def draw(self):
+        # Fill background color
+        for item in self._items:
+            item.draw()
 
 class MenuItem(object):
-    def __init__(self, title, action, location, size):
+    def __init__(self, game, title, action, coords, dimensions, fill="#00ff00", color="#000000", outline="#000000", font=("Monospace",12,"")):
+        self._game = game
         self._title = action
         self._action = action
-        self._location = location
-        self._size = size
+        self._coords = coords
+        self._dimensions = dimensions
+        self._fill = fill
+        self._color = color
+        self._outline = outline
+        self._font = font
+        self._tag = None
     def on_click(self, click_pos):
-        if self._location[0] < click_pos[0] < (self._location[0] + self._size[0]) and self._location[1] < click_pos[1] < (self._location[1] + self._size[1]):
+        if self._coords[0] < click_pos[0] < (self._coords[0] + self._dimensions[0]) and self._coords[1] < click_pos[1] < (self._coords[1] + self._dimensions[1]):
             self._action()
             return True
         else:
             return False
+    def draw(self):
+        self._tag = self._game._canvas.create_rectangle(
+            self._coords[0],
+            self._coords[1],
+            self._dimensions[0] + self._coords[0],
+            self._dimensions[1] + self._coords[1],
+            fill=self._fill,
+            outline=self._outline
+        )
+        # Draw text (Don't have time today, have to commit changes
+        )
