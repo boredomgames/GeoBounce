364107f1a34e37648f84d008f4b549e38d0c0c79
Contributor P <p@example.com>
2020-03-12 19:45:17 +0000

    Not done, work on later, please don't touch

--- GeoBounce/__main__.py
+++ GeoBounce/__main__.py
@@ -5,13 +5,15 @@ import sys
 
 def main():
     game = Game()
+    # Game Selector
+    game.play() # New Location, to update screen at menu (idk if this si correct)
     if len(sys.argv) == 2 and sys.argv[1] == "lvl1":
         level = Level(game, "Level 1", level1)
     else:
         level = Level(game, "Level 2", level2)
     level.generate()
     level.draw()
-    game.play()
+    #game.play() # Old Location
     level.run()
 
 if __name__ == "__main__":
