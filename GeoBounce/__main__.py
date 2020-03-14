f5f67efb9e314314bc9d40a80429fdec2ec4e735
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-03-13 21:14:37 -0700

    Use new systems in __main__.py

--- GeoBounce/__main__.py
+++ GeoBounce/__main__.py
@@ -1,20 +1,47 @@
 from .game import Game
 from .level import LevelOld, Level
 from .levels import level1, level2
+from .gui import GUI, Button, Label
 import sys
 
-def main():
-    game = Game()
-    # Game Selector
-    game.play() # New Location, to update screen at menu (idk if this si correct)
-    if len(sys.argv) == 2 and sys.argv[1] == "lvl1":
-        level = Level(game, "Level 1", level1)
-    else:
-        level = Level(game, "Level 2", level2)
+GUI_DRAWN = False
+GAME_RUNNING = False
+
+def level_runner(gui, game, name, level):
+    global GAME_RUNNING, GUI_DRAWN
+    gui.delete()
+    GUI_DRAWN = False
+
+    level = Level(game, name, level)
     level.generate()
     level.draw()
-    #game.play() # Old Location
     level.run()
+    level.cleanup()
+
+    GAME_RUNNING = False
+
+def main():
+    global GUI_DRAWN
+
+    game = Game()
+    game.play()
+    gui = GUI(game, (200, 10), (400, 590), widgets=[
+        [
+            Label("GeoBounce", style={"font_size": 100})
+        ],
+        [
+            Button("Level 1", command=lambda e: level_runner(gui, game, "Level 1", level1)),
+            Button("Level 2", command=lambda e: level_runner(gui, game, "Level 2", level2)),
+        ]
+    ])
+
+    while True:
+        if not GAME_RUNNING:
+            game.update()
+
+            if not GUI_DRAWN:
+                GUI_DRAWN = True
+                gui.draw()
 
 if __name__ == "__main__":
     main()
