34e56790e097078f85e781768a1a0ec18928ed0d
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-03-15 14:12:53 -0700

    Draw on demand optimizations?

--- GeoBounce/level.py
+++ GeoBounce/level.py
@@ -39,15 +39,22 @@ class Level(object):
         self._items = items
 
         self._obstacles = []
+        self._obstacles_drawn = []
         self._obstacles_tags = []
         self._surfaces = []
+        self._surfaces_drawn = []
         self._surfaces_tags = []
         self._rewards = []
+        self._rewards_drawn = []
         self._rewards_tags = []
         self._player = []
+        self._player_drawn = []
         self._player_tag = []
         self._goal = []
+        self._goal_drawn = []
         self._goal_tag = []
+
+        self._current_coords = 0
 
         self._player_jumping = False
         self._player_stuck = False
@@ -70,6 +77,12 @@ class Level(object):
     def generate(self):
         for item in self._items:
             self.generate_sprite(item)
+
+        self._obstacles.sort(key=lambda item: item.bbox()[0])
+        self._surfaces.sort(key=lambda item: item.bbox()[0])
+        self._rewards.sort(key=lambda item: item.bbox()[0])
+        self._player.sort(key=lambda item: item.bbox()[0])
+        self._goal.sort(key=lambda item: item.bbox()[0])
 
     def generate_sprite(self, item):
         types = {
@@ -121,27 +134,92 @@ class Level(object):
             raise ValueError(f'{item["sprite_type"]} is not a sprite type')
 
     def draw(self):
-        for obstacle in self._obstacles:
+        self._gui.draw()
+        self.draw_current()
+
+    def draw_current(self):
+        window_width, window_height = self._game.dimensions
+
+        obstacles = filter(lambda item: item.bbox()[0] <= self._current_coords + window_width + 100, self._obstacles)
+
+        for obstacle in obstacles:
+            del self._obstacles[self._obstacles.index(obstacle)]
             obstacle.draw()
+            self._obstacles_drawn.append(obstacle)
             self._obstacles_tags.append(obstacle._tag)
 
-        for surface in self._surfaces:
+            position = obstacle.position()
+            obstacle.teleport((position[0] - self._current_coords, position[1]))
+
+        surfaces = filter(lambda item: item.bbox()[0] <= self._current_coords + window_width + 100, self._surfaces)
+
+        for surface in surfaces:
+            del self._surfaces[self._surfaces.index(surface)]
             surface.draw()
+            self._surfaces_drawn.append(surface)
             self._surfaces_tags.append(surface._tag)
 
-        for reward in self._rewards:
+            position = surface.position()
+            surface.teleport((position[0] - self._current_coords, position[1]))
+
+        rewards = filter(lambda item: item.bbox()[0] <= self._current_coords + window_width + 100, self._rewards)
+
+        for reward in rewards:
+            del self._rewards[self._rewards.index(reward)]
             reward.draw()
+            self._rewards_drawn.append(reward)
             self._rewards_tags.append(reward._tag)
 
-        for player in self._player:
+            position = reward.position()
+            reward.teleport((position[0] - self._current_coords, position[1]))
+
+        players = filter(lambda item: item.bbox()[0] <= self._current_coords + window_width + 100, self._player)
+
+        for player in players:
+            del self._player[self._player.index(player)]
             player.draw()
+            self._player_drawn.append(player)
             self._player_tag.append(player._tag)
 
-        for goal in self._goal:
+            position = player.position()
+            player.teleport((position[0] - self._current_coords, position[1]))
+
+        goals = filter(lambda item: item.bbox()[0] <= self._current_coords + window_width + 100, self._goal)
+
+        for goal in goals:
+            del self._goal[self._goal.index(goal)]
             goal.draw()
+            self._goal_drawn.append(goal)
             self._goal_tag.append(goal._tag)
 
-        self._gui.draw()
+            position = goal.position()
+            goal.teleport((position[0] - self._current_coords, position[1]))
+
+    def delete_old(self):
+        while self._obstacles_drawn and self._obstacles_drawn[0].bbox()[2] < 0:
+            self._obstacles_drawn[0].delete()
+            del self._obstacles_drawn[0]
+            del self._obstacles_tags[0]
+
+        while self._surfaces_drawn and self._surfaces_drawn[0].bbox()[2] < 0:
+            self._surfaces_drawn[0].delete()
+            del self._surfaces_drawn[0]
+            del self._surfaces_tags[0]
+
+        while self._rewards_drawn and self._rewards_drawn[0].bbox()[2] < 0:
+            self._rewards_drawn[0].delete()
+            del self._rewards_drawn[0]
+            del self._rewards_tags[0]
+
+        while self._player_drawn and self._player_drawn[0].bbox()[2] < 0:
+            self._player_drawn[0].delete()
+            del self._player_drawn[0]
+            del self._player_tag[0]
+
+        while self._goal_drawn and self._goal_drawn[0].bbox()[2] < 0:
+            self._goal_drawn[0].delete()
+            del self._goal_drawn[0]
+            del self._goal_tag[0]
 
     def run(self):
         self._game.name = self._name
@@ -153,12 +231,16 @@ class Level(object):
             self._player_jumping = True
 
         while not self._end:
+            self._current_coords += SPEED
+
             self._timer.begin()
             self.move()
             self.player_jump()
             self.collide()
             self.player_move()
             self.update_points()
+            self.draw_current()
+            self.delete_old()
             self._game.update()
 
             if self._player_dead and self._testmode:
@@ -211,32 +293,34 @@ class Level(object):
         )
 
     def delete(self):
-        for obstacle in self._obstacles:
+        for obstacle in self._obstacles_drawn:
             obstacle.delete()
 
-        for surface in self._surfaces:
+        for surface in self._surfaces_drawn:
             surface.delete()
 
-        for reward in self._rewards:
+        for reward in self._rewards_drawn:
             reward.delete()
 
-        for goal in self._goal:
+        for goal in self._goal_drawn:
             goal.delete()
 
-        self._player[0].delete()
+        for player in self._player_drawn:
+            player.delete()
+
         self._gui.delete()
 
     def move(self):
-        for obstacle in self._obstacles:
+        for obstacle in self._obstacles_drawn:
             obstacle.move((-SPEED, 0))
 
-        for surface in self._surfaces:
+        for surface in self._surfaces_drawn:
             surface.move((-SPEED, 0))
 
-        for reward in self._rewards:
+        for reward in self._rewards_drawn:
             reward.move((-SPEED, 0))
 
-        for goal in self._goal:
+        for goal in self._goal_drawn:
             goal.move((-SPEED, 0))
 
     def player_jump(self):
@@ -247,10 +331,10 @@ class Level(object):
 
             if self._player_jump_frames < JUMP_FRAMES / 2:
                 # move player up if it is less than halfway through
-                self._player[0].move((0, -JUMP_PATH[self._player_jump_frames]))
+                self._player_drawn[0].move((0, -JUMP_PATH[self._player_jump_frames]))
             else:
                 # otherwise move player down
-                self._player[0].move(
+                self._player_drawn[0].move(
                     (
                         0,
                         JUMP_PATH[
@@ -265,7 +349,7 @@ class Level(object):
             self._player_jumping = False
 
     def player_move(self):
-        player = self._player[0]
+        player = self._player_drawn[0]
         player_bbox = player.bbox()
         window_width, window_height = self._game.dimensions
 
@@ -304,7 +388,7 @@ class Level(object):
         # reset player stuck
         self._player_gravity = True
 
-        for item in self._player[0].collide():
+        for item in self._player_drawn[0].collide():
             # end the game when player collides into an obstacle
             if item in self._obstacles_tags:
                 self._player_dead = True
@@ -312,9 +396,9 @@ class Level(object):
 
             if item in self._surfaces_tags:
                 # bounding box for the collided surface
-                surface = self._surfaces[self._surfaces_tags.index(item)].bbox()
+                surface = self._surfaces_drawn[self._surfaces_tags.index(item)].bbox()
                 # bounding box for player
-                player = self._player[0].bbox()
+                player = self._player_drawn[0].bbox()
 
                 if player[2] <= surface[0]:
                     # player is stuck when blocked by a surface
@@ -325,7 +409,7 @@ class Level(object):
 
                     if not player[3] > surface[3]:
                         # move player to top of surface unless it was below
-                        self._player[0].teleport(
+                        self._player_drawn[0].teleport(
                             (player[0], surface[1] - (player[3] - player[1]))
                         )
                     else:
@@ -336,12 +420,12 @@ class Level(object):
 
             if item in self._rewards_tags:
                 # destroy reward if player collided with it
-                reward = self._rewards[self._rewards_tags.index(item)]
+                reward = self._rewards_drawn[self._rewards_tags.index(item)]
                 reward.delete()
                 # give the player one point
                 self._player_points += 1
                 # remove the reward from rewards
-                del self._rewards[self._rewards_tags.index(item)]
+                del self._rewards_drawn[self._rewards_tags.index(item)]
                 # remove the reward tag form reward tags
                 del self._rewards_tags[self._rewards_tags.index(item)]
 
