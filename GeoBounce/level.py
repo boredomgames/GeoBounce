4686a7a25870025a3cef9de6b1787bde9ef9d1b5
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-03-17 16:55:48 -0700

    Fix player not dying when out of window bug

--- GeoBounce/level.py
+++ GeoBounce/level.py
@@ -297,6 +297,8 @@ class Level(object):
         if player_bbox[2] <= 0:
             # end the game
             self._end = True
+            # player is dead
+            self._player_dead = True
 
     def collide(self):
         # reset player gravity
